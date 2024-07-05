ffd0c35d60162a69b58030ad8fa84e41b0559f54
Pranav Pakanati <pranav.pakanati@gmail.com>
2024-07-05 13:39:11 -0500

    add ca to drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'drizzle-kit';
+import { readFileSync } from 'fs';
 
 export default defineConfig({
     dialect: 'postgresql',
     schema: './src/lib/server/db/schema/*',
     out: './drizzle',
     dbCredentials: {
-        url: process.env.DATABASE_URL ?? ''
+        url: process.env.DATABASE_URL ?? '',
+        ssl: {
+            rejectUnauthorized: true,
+            ca: readFileSync('.env.pg-cert.pem').toString()
+        }
     },
     verbose: true,
     strict: true
