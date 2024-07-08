f8c00628d6668fe2dda2e1054e405d4d16bfe877
Pranav Pakanati <pranav.pakanati@gmail.com>
2024-07-07 23:29:53 -0500

    dashboard skeleton

--- src/lib/components/dashboard/LessonItem.svelte
+++ src/lib/components/dashboard/LessonItem.svelte
@@ -1,46 +1,41 @@
 <script lang="ts">
     export let text: string;
-    export let current: boolean;
+    export let current: boolean = false;
 </script>
 
 <div class="text-container">
-    <p class={current ? "current" : ""}>{text}</p>
+    <p class={current ? 'current' : ''}>{text}</p>
 </div>
 
 <style lang="scss">
-
-    .text-container{
+    .text-container {
         display: flex;
-        width: 20.5rem;
+        width: 100%;
         height: 2.5rem;
         padding: 0rem var(--padding-sm, 0.5rem);
         justify-content: space-between;
         align-items: center;
         flex-shrink: 0;
         border-radius: 0.5rem;
+        transition: 300ms box-shadow ease;
 
-
-        &:hover{
+        &:hover {
             box-shadow: var(--shadow-sm-medium);
         }
 
         p {
-        display: flex;
-        height: 1.597rem;
-        flex-direction: column;
-        justify-content: center;
-        flex: 1 0 0;
-        @include paragraph-xl;
-        color: var(--colors-gray, #565656);
+            display: flex;
+            height: 1.597rem;
+            flex-direction: column;
+            justify-content: center;
+            flex: 1 0 0;
+            @include paragraph-xl;
+            color: var(--colors-gray, #565656);
+
+            &.current {
+                @include paragraph-xl-b;
+                color: var(--colors-dark-gray, #383838);
+            }
         }
-        .current{
-        @include paragraph-xl-b;
-        color: var(--colors-dark-gray, #383838);
-        }
-        
-        
     }
-    
-
 </style>
-
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+    import LogoText from '$lib/components/LogoText.svelte';
+</script>
+
+<div class="container">
+    <nav>
+        <LogoText />
+    </nav>
+    <main>
+        <aside class="contents"></aside>
+        <article></article>
+    </main>
+</div>
+
+<style lang="scss">
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: var(--padding-sm);
+        gap: var(--padding-sm);
+        width: 100%;
+        height: 100vh;
+        background: linear-gradient(to right top, #9309de 0%, rgba(147, 9, 222, 0.5) 100%),
+            var(--colors-background, #fff);
+
+        nav {
+            height: 3.75rem;
+            width: 100%;
+            background: var(--background);
+            border-radius: 0.5rem;
+            display: flex;
+            align-items: center;
+            padding: 0 var(--padding-md);
+        }
+
+        main {
+            display: flex;
+            width: 100%;
+            height: 100%;
+            gap: var(--padding-sm);
+
+            .contents {
+                display: flex;
+                width: 20rem;
+                padding: var(--page-padding);
+                flex-direction: column;
+                align-items: flex-start;
+                gap: 3rem;
+                flex-shrink: 0;
+                align-self: stretch;
+                border-radius: 0.5rem;
+                background: var(--background);
+                overflow: auto;
+            }
+
+            article {
+                width: 100%;
+                height: 100%;
+                padding: var(--page-padding);
+                background: var(--background);
+                border-radius: 0.5rem;
+            }
+        }
+    }
+</style>
