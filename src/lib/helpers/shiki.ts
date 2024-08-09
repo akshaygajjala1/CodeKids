25c248ec64c80df6022cf6b1205c71c889bcdd24
Pranav <pranav.pakanati@gmail.com>
2024-08-08 22:07:15 -0500

    only bundle required themes / langs for shiki

--- src/lib/helpers/shiki.ts
+++ src/lib/helpers/shiki.ts
@@ -1,6 +1,17 @@
-import { createHighlighter } from 'shiki';
+import { createHighlighterCore, type HighlighterCore } from "shiki/core";
+import snazzyLight from 'shiki/themes/snazzy-light.mjs';
+import py from 'shiki/langs/py.mjs';
+import python from 'shiki/langs/python.mjs';
 
-export const highlighter = await createHighlighter({
-    themes: ['snazzy-light'],
-    langs: ['py', 'python']
-});
+export let highlighter: HighlighterCore;
+// top-level await hack
+(async () => highlighter = await createHighlighterCore({
+    themes: [
+        snazzyLight
+    ],
+    langs: [
+        py,
+        python
+    ],
+    loadWasm: import('shiki/wasm')
+}))();
