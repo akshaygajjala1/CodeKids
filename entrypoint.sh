4aaff6b8a13966995936b376e0408be5f3040634
Pranav <pranav.pakanati@gmail.com>
2024-08-09 17:49:58 -0500

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-envsubst '\${PORT}' < /etc/nginx/conf.d/default.conf/template > /etc/nginx/conf.d/default.conf &&
+envsubst '\${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf &&
 nginx -g 'daemon off;' & 
 cd server && gunicorn -w 1 --threads 10 -b 127.0.0.1:5000 app:app &
 PORT=8000 node build &
